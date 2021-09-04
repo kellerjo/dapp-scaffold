1926606fe267d897e5670a59dfed8344e0817893
kellerjo <johannes.keller2@gmx.de>
2021-09-05 00:01:15 +0200

    Updates

---
+++ precache-manifest.e8b321bcae3cc4d880d1108b7a27ee07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ec3e1a344f6a6cd7caf3d60e5779fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1a4d1a1eb46e1b83042",
+    "url": "./static/css/main.d27ecd4c.chunk.css"
+  },
+  {
+    "revision": "6d19bbd154fb7984b880",
+    "url": "./static/js/2.2d8f2351.chunk.js"
+  },
+  {
+    "revision": "ed798670beb8e30736afbb766722b31a",
+    "url": "./static/js/2.2d8f2351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a4d1a1eb46e1b83042",
+    "url": "./static/js/main.8c03909d.chunk.js"
+  },
+  {
+    "revision": "da503e7975902b161ea8",
+    "url": "./static/js/runtime-main.7d63d1b5.js"
+  },
+  {
+    "revision": "a4e80c1e977416ec45d808a3268892b7",
+    "url": "./static/media/builton.a4e80c1e.svg"
+  }
+]);
